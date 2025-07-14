8ddf1fdbdbbb2900a8f9ed339ab63f0d0aee3350
prachinayak03 <nprachi860@gmail.com>
2025-07-14 18:12:59 +0530

    Update Additems.jsx

--- src/Components/Additems.jsx
+++ src/Components/Additems.jsx
@@ -6,13 +6,21 @@ const [date , setDate]= useState();
 
 const Addbtnhandler = ()=>{
   AddHandler(name , date)
+  setDate('')
+  setName("")
 }
   return (
     <div className='d-flex gap-3 container-fluid  p-3  '>
-        <input type="text" onChange={el => setName(el.target.value)} 
-        placeholder="Addlist....."className="form-control p-2 rounded" />
-        <input type="date" onChange={el => setDate(el.target.value)} 
-        className="form-control p-2 rounded" />
+        <input type="text" 
+        onChange={el => setName(el.target.value)} 
+        placeholder="Addlist....."
+        className="form-control p-2 rounded"
+        value={name} />
+        <input type="date" 
+        onChange={el => setDate(el.target.value)} 
+        className="form-control p-2 rounded" 
+        value={date}
+        />
         <button onClick={Addbtnhandler}className="btn-primary btn px-3">+</button>
 
 
