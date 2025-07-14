7c8863a5cc88fef897b01f4c7b7a560e4b6a8eb0
prachinayak03 <nprachi860@gmail.com>
2025-07-14 18:55:59 +0530

    Create Message.jsx

---
+++ src/Components/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+      <h1 className='text-center text-light py-2'>Enjoy your day!</h1>
+    </div>
+  )
+}
+
+export default Message
