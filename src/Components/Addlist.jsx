17ddfb53843df1c0c701e565bef2db78a99ba636
prachinayak03 <nprachi860@gmail.com>
2025-07-14 18:09:52 +0530

    Update Addlist.jsx

--- src/Components/Addlist.jsx
+++ src/Components/Addlist.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import TodoItems from './TodoItems'
 
-const Addlist = ({items}) => {
+const Addlist = ({items , deleteHandler}) => {
     
   return (
     <div className=' py-1 px-2 mt-3 text-light'>
         {
-            items.map (el => <TodoItems key={el.name} name={el.name} date={el.date}/> )
+            items.map (el => <TodoItems key={el.name} name={el.name} date={el.date} 
+              deleteHandler={deleteHandler}/> )
         }
       
 
