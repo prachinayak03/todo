123cfee930100968fe34e3adcec2e60fe4f9ec14
prachinayak03 <nprachi860@gmail.com>
2025-07-14 18:11:50 +0530

    Update TodoItems.jsx

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { MdDelete } from "react-icons/md";
-const TodoItems = ({name ,date }) => {
+const TodoItems = ({name ,date , deleteHandler}) => {
   return (
     <div className='d-flex gap-3 justify-content-between w-100 align-items-center fw-bold  bg-light text-dark rounded p-2 my-2'>
         <p className='pt-2 w-25'>{name}</p>
 
         <p className='pt-2 w-25'>{date}</p>
 
-        <button className="btn-danger btn px-3"><MdDelete/></button>
+        <button onClick={()=>deleteHandler(name)} className="btn-danger btn px-3"><MdDelete/></button>
       
     </div>
   )
